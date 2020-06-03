6e3a22625457023790db4b6bd5a6c6e9a8986874
kshitiz kamal <skilite007@gmail.com>
2020-06-03 23:17:15 +0530

    setup ejs

--- README.md
+++ README.md
@@ -2,13 +2,13 @@
 
 Full stack web dev files
 Technologies and frameworks learned:
-HTML5
-CSS3
-Bootstrap4
-Advanced DOM
-JS
-jQuery
-Nodejs
-Express
-APIs
-Version control
+- HTML5
+- CSS3
+- Bootstrap4
+- Advanced DOM
+- JS
+- jQuery
+- Nodejs
+- Express
+- APIs
+- Version control
---
+++ ejs/app.js
@@ -0,0 +1,20 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+app.set("view engine", "ejs");
+app.get("/", function (req, res) {
+  var today = new Date();
+  var currentDay = today.getDay();
+  var day = "";
+  if (currentDay == 0 || currentDay == 6) {
+    day = "weekend";
+  } else {
+    day = "weekday";
+  }
+  res.render("list", { kindOfDay: day });
+});
+
+app.listen(3000, function () {
+  console.log("server started on port 3000");
+});
